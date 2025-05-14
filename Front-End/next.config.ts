249f88ec5860901a00cdf51b9d916cdca3f5ccc4
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2025-05-15 01:02:51 +0200

    deployment

--- Front-End/next.config.ts
+++ Front-End/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',          
+  basePath: '/web-innova-physics',
 };
 
 export default nextConfig;
