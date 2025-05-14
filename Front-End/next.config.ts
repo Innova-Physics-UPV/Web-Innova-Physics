b125bfeeb6ffc93b7dcdc989b7e6746a1641b6d8
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2025-05-15 01:14:05 +0200

    images unoptimazed

--- Front-End/next.config.ts
+++ Front-End/next.config.ts
@@ -3,15 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
-  // Ignora errores de ESLint en el build
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Ignora errores de TypeScript en el build
   typescript: {
     ignoreBuildErrors: true,
   },
-  basePath: '/web-innova-physics',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
